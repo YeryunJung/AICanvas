0b74924ef8504bc376b2fb04785ccdac77da016c
YeryunJung <2bites@naver.com>
2024-02-13 13:58:40 +0900

    refactor: BGMProvider 수정

--- client/src/sounds/MusicContext.tsx
+++ client/src/sounds/MusicContext.tsx
@@ -1,4 +1,10 @@
-import React, { createContext, useState, useContext, useMemo } from 'react';
+import React, {
+  createContext,
+  useState,
+  useContext,
+  useMemo,
+  useCallback,
+} from 'react';
 import useSound from 'use-sound';
 import drawBGM from '../assets/sound/background/cheeky_monkey_fun_app_playful_cheeky.mp3';
 import stageBGM from '../assets/sound/background/African_fun_long.mp3';
@@ -6,10 +12,13 @@ import mainBGM from '../assets/sound/background/mr_clown.mp3';
 
 const BGMContext = createContext<
   | {
+      mainPlaying: boolean;
+      stagePlaying: boolean;
+      drawPlaying: boolean;
+      isMuted: boolean;
       startBGM: (bgmType: 'main' | 'stage' | 'draw') => void;
       stopBGM: () => void;
-      toggleMute: () => void; // 무음 모드를 토글하기 위한 함수
-      isMuted: boolean; // 무음 모드 상태를 나타내는 변수
+      toggleMute: () => void; // 무음모드 토글 함수
     }
   | undefined
 >(undefined);
@@ -17,6 +26,10 @@ const BGMContext = createContext<
 export const BGMProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
+  const [mainPlaying, setMainPlaying] = useState(false);
+  const [stagePlaying, setStagePlaying] = useState(false);
+  const [drawPlaying, setDrawPlaying] = useState(false);
+  const [isMuted, setIsMuted] = useState(false);
   const [playMain, { stop: stopMain }] = useSound(mainBGM, {
     volume: 0.4,
     loop: true,
@@ -29,98 +42,92 @@ export const BGMProvider: React.FC<{ children: React.ReactNode }> = ({
     volume: 0.4,
     loop: true,
   });
-  const [mainPlaying, setMainPlaying] = useState(false);
-  const [stagePlaying, setStagePlaying] = useState(false);
-  const [drawPlaying, setDrawPlaying] = useState(false);
-  const [isMuted, setIsMuted] = useState(false); // 초기값은 무음 모드가 아님
 
-  // useMemo를 사용하여 value 객체를 최적화
+  const toggleMute = useCallback(() => {
+    setIsMuted((prevIsMuted) => !prevIsMuted);
+  }, []);
+
+  const startBGM = useCallback(
+    (bgmType: 'main' | 'stage' | 'draw') => {
+      if (!isMuted) {
+        if (bgmType === 'main') {
+          if (!mainPlaying) {
+            if (stagePlaying) {
+              stopStage();
+              setStagePlaying(false);
+            }
+            if (drawPlaying) {
+              stopDraw();
+              setDrawPlaying(false);
+            }
+            playMain();
+            setMainPlaying(true);
+          }
+        } else if (bgmType === 'stage') {
+          if (!stagePlaying) {
+            if (mainPlaying) {
+              stopMain();
+              setMainPlaying(false);
+            }
+            if (drawPlaying) {
+              stopDraw();
+              setDrawPlaying(false);
+            }
+            playStage();
+            setStagePlaying(true);
+          }
+        } else if (bgmType === 'draw') {
+          if (!drawPlaying) {
+            if (mainPlaying) {
+              stopMain();
+              setMainPlaying(false);
+            }
+            if (stagePlaying) {
+              stopStage();
+              setStagePlaying(false);
+            }
+            playDraw();
+            setDrawPlaying(true);
+          }
+        }
+      }
+    },
+    [isMuted, mainPlaying, stagePlaying, drawPlaying],
+  );
+
+  const stopBGM = useCallback(() => {
+    if (mainPlaying) {
+      stopMain();
+      setMainPlaying(false);
+    }
+    if (stagePlaying) {
+      stopStage();
+      setStagePlaying(false);
+    }
+    if (drawPlaying) {
+      stopDraw();
+      setDrawPlaying(false);
+    }
+  }, [mainPlaying, stagePlaying, drawPlaying]);
+
   const value = useMemo(
     () => ({
       mainPlaying,
       stagePlaying,
       drawPlaying,
       isMuted,
-      startBGM: (bgmType: 'main' | 'stage' | 'draw') => {
-        console.log('play', isMuted);
-        if (!isMuted) {
-          switch (bgmType) {
-            case 'main':
-              if (!mainPlaying) {
-                if (stagePlaying) {
-                  stopStage();
-                  setStagePlaying(false);
-                }
-                if (drawPlaying) {
-                  stopDraw();
-                  setDrawPlaying(false);
-                }
-                playMain();
-                setMainPlaying(true);
-              }
-              break;
-            case 'stage':
-              if (!stagePlaying) {
-                if (mainPlaying) {
-                  stopMain();
-                  setMainPlaying(false);
-                }
-                if (drawPlaying) {
-                  stopDraw();
-                  setDrawPlaying(false);
-                }
-                playStage();
-                setStagePlaying(true);
-              }
-              break;
-            case 'draw':
-              if (!drawPlaying) {
-                if (mainPlaying) {
-                  stopMain();
-                  setMainPlaying(false);
-                }
-                if (stagePlaying) {
-                  stopStage();
-                  setStagePlaying(false);
-                }
-                playDraw();
-                setDrawPlaying(true);
-              }
-              break;
-            default:
-              break;
-          }
-        }
-      },
-      stopBGM: () => {
-        if (mainPlaying) {
-          stopMain();
-          setMainPlaying(false);
-        }
-        if (stagePlaying) {
-          stopStage();
-          setStagePlaying(false);
-        }
-        if (drawPlaying) {
-          stopDraw();
-          setDrawPlaying(false);
-        }
-      },
-      toggleMute: () => {
-        setIsMuted(!isMuted); // 무음 모드를 토글
-      },
+      startBGM,
+      stopBGM,
+      toggleMute,
     }),
     [
       mainPlaying,
       stagePlaying,
       drawPlaying,
-      playMain,
-      stopMain,
-      playStage,
-      stopStage,
-      playDraw,
-      stopDraw,
       isMuted,
+      startBGM,
+      stopBGM,
+      toggleMute,
     ],
   );
 
