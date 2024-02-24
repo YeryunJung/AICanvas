10714cd4cf0374ad84e1d0e5b8627d5bdd86a0b4
YeryunJung <2bites@naver.com>
2024-02-25 03:07:39 +0900

    chore: 서비스 워커 폰트 캐싱 수정

--- client/public/service-worker.js
+++ client/public/service-worker.js
@@ -1,14 +1,31 @@
 /* eslint-disable */
+const CACHE_NAME = 'ai-canvas-cache-v1';
+const immutableRequests = [
+  'https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_20-07@1.0/TmoneyRoundWindExtraBold.woff',
+  'https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_20-07@1.0/TmoneyRoundWindRegular.woff',
+  'https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2108@1.1/SBAggroB.woff',
+];
+
 // install event
 self.addEventListener('install', (e) => {
   console.log('서비스워커 설치!');
   e.waitUntil(
-    caches.open('font-cache').then((cache) => {
-      return cache.addAll([
-        'https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_20-07@1.0/TmoneyRoundWindExtraBold.woff',
-        'https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_20-07@1.0/TmoneyRoundWindRegular.woff',
-        'https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2108@1.1/SBAggroB.woff',
-      ]);
+    caches.open(CACHE_NAME).then((cache) => {
+      const newImmutableRequests = [];
+      return Promise.all(
+        immutableRequests.map((url) => {
+          return caches.match(url).then((response) => {
+            if (response) {
+              return cache.put(url, response);
+            } else {
+              newImmutableRequests.push(url);
+              return Promise.resolve();
+            }
+          });
+        }),
+      ).then(() => {
+        return cache.addAll(newImmutableRequests);
+      });
     }),
   );
 });
@@ -20,10 +37,13 @@ self.addEventListener('activate', (e) => {
 
 // fetch event
 self.addEventListener('fetch', (e) => {
-  console.log('Fetch request for: ', e.request.url);
   e.respondWith(
     caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
+      if (response) {
+        console.log('이미 캐싱된 리소스', response);
+        return response;
+      }
+      return fetch(e.request);
     }),
   );
 });
